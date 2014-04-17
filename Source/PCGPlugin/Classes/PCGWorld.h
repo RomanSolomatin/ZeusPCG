7c81087292b4eb2116c649ad4a60b7b765d4aebe
jkeywo <jkeywo@yahoo.co.uk>
2014-04-17 15:17:37 +0100

    Initial commit

---
+++ Source/PCGPlugin/Classes/PCGWorld.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "PCGWorld.generated.h"
+
+
+UCLASS()
+class UPCGWorld : public UObject
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+
+private:
+
+};
+
+
---
+++ Source/PCGPlugin/Private/PCGWorld.cpp
@@ -0,0 +1,10 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#include "PCGPluginPrivatePCH.h"
+#include "PCGWorld.h"
+
+
+UPCGWorld::UPCGWorld(const FPostConstructInitializeProperties& PCIP)
+	: Super( PCIP )
+{
+}
